0bf02c64f36e0eaa850d5993dc4a37fc0364058b
NathanFallet <contact@nathanfallet.me>
2023-11-25 11:56:39 +0100

    Fixing appleMain compilation

--- src/appleMain/kotlin/me/nathanfallet/i18n/usecases/localization/TranslateUseCase.kt
+++ src/appleMain/kotlin/me/nathanfallet/i18n/usecases/localization/TranslateUseCase.kt
@@ -1,5 +1,7 @@
 package me.nathanfallet.i18n.usecases.localization
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.cstr
 import me.nathanfallet.usecases.localization.ITranslateUseCase
 import me.nathanfallet.usecases.localization.Locale
 import platform.Foundation.NSBundle
@@ -11,6 +13,7 @@ actual class TranslateUseCase(
     private val tableName: String = "Localizable"
 ) : ITranslateUseCase {
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun invoke(input1: Locale, input2: String, input3: List<String>): String {
         val localizedString = NSBundle.mainBundle
             .localizedStringForKey(input2, input2, tableName)
@@ -25,55 +28,70 @@ actual class TranslateUseCase(
 
         return when (input3.size) {
             0 -> NSString.stringWithFormat(localizedString)
-            1 -> NSString.stringWithFormat(localizedString, input3[0])
-            2 -> NSString.stringWithFormat(localizedString, input3[0], input3[1])
-            3 -> NSString.stringWithFormat(localizedString, input3[0], input3[1], input3[2])
-            4 -> NSString.stringWithFormat(localizedString, input3[0], input3[1], input3[2], input3[3])
-            5 -> NSString.stringWithFormat(localizedString, input3[0], input3[1], input3[2], input3[3], input3[4])
+            1 -> NSString.stringWithFormat(localizedString, input3[0].cstr)
+            2 -> NSString.stringWithFormat(localizedString, input3[0].cstr, input3[1].cstr)
+            3 -> NSString.stringWithFormat(localizedString, input3[0].cstr, input3[1].cstr, input3[2].cstr)
+            4 -> NSString.stringWithFormat(
+                localizedString,
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr
+            )
+
+            5 -> NSString.stringWithFormat(
+                localizedString,
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr,
+                input3[4].cstr
+            )
+
             6 -> NSString.stringWithFormat(
                 localizedString,
-                input3[0],
-                input3[1],
-                input3[2],
-                input3[3],
-                input3[4],
-                input3[5]
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr,
+                input3[4].cstr,
+                input3[5].cstr
             )
 
             7 -> NSString.stringWithFormat(
                 localizedString,
-                input3[0],
-                input3[1],
-                input3[2],
-                input3[3],
-                input3[4],
-                input3[5],
-                input3[6]
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr,
+                input3[4].cstr,
+                input3[5].cstr,
+                input3[6].cstr
             )
 
             8 -> NSString.stringWithFormat(
                 localizedString,
-                input3[0],
-                input3[1],
-                input3[2],
-                input3[3],
-                input3[4],
-                input3[5],
-                input3[6],
-                input3[7]
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr,
+                input3[4].cstr,
+                input3[5].cstr,
+                input3[6].cstr,
+                input3[7].cstr
             )
 
             9 -> NSString.stringWithFormat(
                 localizedString,
-                input3[0],
-                input3[1],
-                input3[2],
-                input3[3],
-                input3[4],
-                input3[5],
-                input3[6],
-                input3[7],
-                input3[8]
+                input3[0].cstr,
+                input3[1].cstr,
+                input3[2].cstr,
+                input3[3].cstr,
+                input3[4].cstr,
+                input3[5].cstr,
+                input3[6].cstr,
+                input3[7].cstr,
+                input3[8].cstr
             )
 
             else -> error("Too many arguments.")
